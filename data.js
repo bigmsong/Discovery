4087db06c1a1310c8c870d9d6e6bb2fb7a300ffb
Paul Song <bigmsong@gmail.com>
2019-11-09 13:30:29 -0500

    deleted extra files

--- data.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var button = document.getElementById("save");
-	button.addEventListener('click', function(response) {
-		chrome.runtime.sendMessage({from: 'popup'});
-	});
-});
